e6e98529fda09e1e715722394496f44e4a87c9a2
Topey <topeydopey@gmail.com>
2025-01-05 00:36:03 +0700

    Arm Setup
    -Set up arm physic with script
    -Added play mode save addon for easy tuning

---
+++ Assets/Scripts/Eyedrop/EyedropArm.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyedropArm : MonoBehaviour
+{
+    public Rigidbody2D upperArm;
+    public Rigidbody2D lowerArm;
+    public float moveSpeed = 5f;
+
+    private void Update()
+    {
+        // Control upper arm rotation
+        if (Input.GetKey(KeyCode.W))
+            upperArm.AddTorque(-moveSpeed, ForceMode2D.Force);
+        if (Input.GetKey(KeyCode.S))
+            upperArm.AddTorque(moveSpeed, ForceMode2D.Force);
+
+        // Control lower arm rotation
+        if (Input.GetKey(KeyCode.A))
+            lowerArm.AddTorque(-moveSpeed, ForceMode2D.Force);
+        if (Input.GetKey(KeyCode.D))
+            lowerArm.AddTorque(moveSpeed, ForceMode2D.Force);
+    }
+}
